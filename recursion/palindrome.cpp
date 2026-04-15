6903fb1a46b37eeda5aa0b15da10ee4d135c5b0d
aarzusharma07 <aarzu257k@gmail.com>
2026-04-15 23:06:56 +0530

    Create palindrome.cpp

---
+++ recursion/palindrome.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int i=0;
+bool palindrome(string s){
+    if(i>s.length()/2){
+        return true;
+    }
+    if(s[i]!=s[s.length()-i-1]){
+        return false;
+    }
+    i++;
+    return palindrome(s);
+}
+int main() {
+    if(palindrome("sir")==true){
+        cout<<"is palindrome";
+    }else{
+        cout<<"not palindrome";
+    }
+    return 0;
+}
