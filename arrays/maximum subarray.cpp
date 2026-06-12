ab3f2ec612adda192000bd381a531375cc199065
Aarzu Sharma <aarzu257k@gmail.com>
2026-06-12 11:13:03 +0530

    Create maximum subarray.cpp

---
+++ arrays/maximum subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int sum=0;
+
+        int maxi=nums[0];
+   
+        for(int i=0;i<nums.size();i++){
+            sum=0;
+
+            for(int j=i;j<nums.size();j++){
+                sum=sum+nums[j];
+                maxi=max(maxi,sum);
+            }
+          
+        }
+        return maxi ;
+    }
+};
